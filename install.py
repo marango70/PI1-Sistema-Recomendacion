29ccf3ac8a777d47faaea73603771328da9d65f0
Maria Isabel Arango M <133439855+marango70@users.noreply.github.com>
2024-07-27 15:08:21 -0500

    Ajustando proyecto

--- install.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-# Instalar dependencias desde requirements.txt
-# subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
-
-# Instalar numpy usando pipwin
-subprocess.check_call(["pipwin", "install", "numpy==1.19.5"])
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ejecutar el script de instalación
-python install.py
-
-# Iniciar la aplicación
-uvicorn main:app --host 0.0.0.0 --port 800
